2cbb0b7a895db41fe833a737b6526fd58b2cf38d
Leila IMANI <leila.imani@ens.uvsq.fr>
2022-11-22 23:26:16 +0100

    Ebauche de struct et fichier pour lecture/écriture de fichier type XYZ.
    Dans le fichier atome.hpp ébauche de struct Position contient les coordonnées x, y, z et Atome contient la position, vitesse, accélération
    et énergie potentielle ainsi que Cellule un tableau qui contient s'il y a un atome.
    Dans le fichier XYZ.cpp ébauche une fonction de lecture de fichier de type XYZ et une fonction d'écriture.

---
+++ XYZ.cpp
@@ -0,0 +1,64 @@
+/** @fichier
+ * Lecture et écriture de fichier .xyz
+ **/
+
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+#include "XYZ.hpp"
+
+//
+Position lireXYZ(std::string source){
+      std::ifstream fichier;
+      fichier.open(source);
+
+      if (not fichier){ throw std::runtime_error("Fichier non trouve : "+source); }
+
+      u32 nbAtome;
+      fichier >> nbAtome;                           //Récupération du <Nombre d'atomes>
+      Position p;
+      p.x.reserve(nbAtome);                         //capacity=nbAtome
+      p.y.reserve(nbAtome);
+      p.z.reserve(nbAtome);
+
+      std::string s;
+      std::getline(fichier, s);                      //Récupère et saute la ligne de commentaire
+
+      f64 p_x, p_y, p_z;
+
+      for (int i = 0; i < nbAtome; ++i) {
+            fichier >> s >> p_x >> p_y >> p_z;     //Récupération des <Élément i> <x(i)> <y(i)> <z(i)>
+            p.x.push_back(p_x);
+            p.y.push_back(p_y);
+            p.z.push_back(p_z);
+      }
+      
+      fichier.close();*/
+      return p;
+}
+
+//
+void  ecrireXYZ(Position const &p, std::string cible){
+      std::ofstream fichier;
+      fichier.open(cible);
+
+      fichier << p.x.size() << std::endl;                              //Ecriture du <Nombre d'atomes> 
+ 
+
+      fichier << "Commentaire : " << std::endl;                        //Ecriture de la ligne de commentaire
+ 
+      f64 p_x, p_y, p_z;
+      std::string s = "Élément";
+
+      for (int i = 0; i < p.x.size(); ++i) {
+            p_x = p.x[i];
+            p_y = p.y[i];
+            p_z = p.z[i];
+            fichier << s <<' '<< p_x <<' '<<' '<< p_y <<' '<< p_z;     //Ecriture des <Élément i> <x(i)> <y(i)> <z(i)>
+            fichier << std::endl;
+      }
+      
+      fichier.close();
+}
+
+int main(){ return 0; }
---
+++ XYZ.hpp
@@ -0,0 +1,35 @@
+/** Un fichier .xyz contient typiquement 
+ * la première ligne : le nombre d'atomes,
+ * la seconde ligne : un commentaire 
+ * et la liste des coordonnées cartésiennes (en ångströms Å) des différents atomes.
+ **/
+
+/**    Disposition :
+ *
+ *   <Nombre d'atomes>
+ *   Ligne de commentaire
+ *   <Élément 1> <x(1)> <y(1)> <z(1)>
+ *   <Élément 2> <x(2)> <y(2)> <z(2)>
+ *   …
+ *   <Élément n> <x(n)> <y(n)> <z(n)>
+ *
+ **/
+
+#ifndef XYZ_H
+#define XYZ_H
+
+#include "types.h"
+#include "atome.hpp"
+
+/** Lit un fichier au format .xyz , retourne des atomes
+ * @param source vers un fichier .xyz
+ * @return position des atomes
+ **/
+Position lireXYZ(std::string source);
+
+/**  Écrit un fichier au format .xyz
+ * @param p position des atomes, cible le nom d'un fichier .xyz
+ **/
+void  ecrireXYZ(Position const &p, std::string cible);
+
+#endif //XYZ_H
