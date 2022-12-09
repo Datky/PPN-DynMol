ce6631995d43bf71537e3db1030fa9b25d4d77c1
Leila IMANI <leila.imani@ens.uvsq.fr>
2022-12-09 21:03:33 +0100

    Corection de erreurs

--- interaction.cpp
+++ interaction.cpp
@@ -29,19 +29,17 @@ void Verlet(Particules & at, f64 const& dt, f64 const& r_cut, f64 const& m){
       F->Y = static_cast<f64*>(std::aligned_alloc(sizeof(f64), sizeof(f64)*N));
       F->Z = static_cast<f64*>(std::aligned_alloc(sizeof(f64), sizeof(f64)*N));
 
-      u32 n = at.pos->X.size();
-
-      for(u32 i=0; i<n; ++i){
+      for(u32 i=0; i<N; ++i){
 
             //Mise à zéro de la force
             F->X = F->Y = F->Z = 0;
 
-            for(u32 j=0; j<n; ++j){
+            for(u32 j=0; j<N; ++j){
 
                   //Calcul de la distance entre les atome r_i(t+dt)
-                  r_tmp->X = at.pos->X[i] - at.pos->X[j];
-                  r_tmp->Y = at.pos->Y[i] - at.pos->Y[j];
-                  r_tmp->Z = at.pos->Z[i] - at.pos->Z[j];
+                  r_tmp->X[i] = at.pos->X[i] - at.pos->X[j];
+                  r_tmp->Y[i] = at.pos->Y[i] - at.pos->Y[j];
+                  r_tmp->Z[i] = at.pos->Z[i] - at.pos->Z[j];
             
 
                   //Calcul des positions : p_i(t+dt)
@@ -57,18 +55,18 @@ void Verlet(Particules & at, f64 const& dt, f64 const& r_cut, f64 const& m){
                   //Calcul de la force : F_i(t+dt) et a_i(t+dt)
 
 
-                  r->X = at.pos->X[i] - at.pos->X[j];
-                  r->Y = at.pos->Y[i] - at.pos->Y[j];
-                  r->Z = at.pos->Z[i] - at.pos->Z[j];
-                  r_globale = sqrt( pow(r->X,2.0) + pow(r->Y,2.0) + pow(r->Z,2.0) );
+                  r->X[i] = at.pos->X[i] - at.pos->X[j];
+                  r->Y[i] = at.pos->Y[i] - at.pos->Y[j];
+                  r->Z[i] = at.pos->Z[i] - at.pos->Z[j];
+                  r_globale = sqrt( pow(r->X[i],2.0) + pow(r->Y[i],2.0) + pow(r->Z[i],2.0) );
 
                   if(r_globale>r_cut) {
                         continue;
                   } 
                   else {
-                        F->X += -(Lennard_Jones(r_tmp->X) - Lennard_Jones(r->X)) / r_tmp->X - r->X;
-                        F->Y += -(Lennard_Jones(r_tmp->Y) - Lennard_Jones(r->Y)) / r_tmp->Y - r->Y;
-                        F->Z += -(Lennard_Jones(r_tmp->Z) - Lennard_Jones(r->Z)) / r_tmp->Z - r->Z;
+                        F->X[i] += -(Lennard_Jones(r_tmp->X[i]) - Lennard_Jones(r->X[i])) / (r_tmp->X[i] - r->X[i]);
+                        F->Y[i] += -(Lennard_Jones(r_tmp->Y[i]) - Lennard_Jones(r->Y[i])) / (r_tmp->Y[i] - r->Y[i]);
+                        F->Z[i] += -(Lennard_Jones(r_tmp->Z[i]) - Lennard_Jones(r->Z[i])) / (r_tmp->Z[i] - r->Z[i]);
                   }
 
 
@@ -78,9 +76,9 @@ void Verlet(Particules & at, f64 const& dt, f64 const& r_cut, f64 const& m){
             }
 
             //Calcul des acceélération : a_i(t+dt)
-            at.acc->X[i] = F->X/m;
-            at.acc->Y[i] = F->Y/m;
-            at.acc->Z[i] = F->Z/m;
+            at.acc->X[i] = F->X[i]/m;
+            at.acc->Y[i] = F->Y[i]/m;
+            at.acc->Z[i] = F->Z[i]/m;
 
             //2ième calcul de des vitesses : v_i(t+dt)
             at.vit->X[i] += 0.5*at.acc->X[i]*dt;
