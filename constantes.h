848592af00b3c0b033edb6f8481a24463df5fbb8
Joel Bougron <joel.bougron@ens.uvsq.fr>
2022-12-11 21:46:20 +0100

    Ajout k_b et temperature_cible

--- constantes.h
+++ constantes.h
@@ -8,3 +8,6 @@ f64 dt=pow(10,-1);   //en s
 #define E_0 119.8*1.380649*pow(10,-23)  //en kg.m²/s²
 #define d 3.405*pow(10,-9)    //en m
 #define m 6.6335*pow(10,-26)    //en kg (= 39,948 ± 0,001 u)
+
+#define k_b 1.380649*pow(10,-23) // constante de Boltzman :	k = 1,380649 × 10−23 J.K−1
+#define temperature_cible 10    // degrés celsius
