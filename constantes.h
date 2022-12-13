e5315e0f8645f3633da79090804000971e79ac8a
Leila IMANI <leila.imani@ens.uvsq.fr>
2022-12-13 10:44:41 +0100

    Changement d'unite des constantes

--- constantes.h
+++ constantes.h
@@ -2,16 +2,16 @@
 
 #define N 10000
 #define nb_iteration 10
-f64 dt=pow(10,-15);   //en s (= 1 fm)
+f64 dt=1;   //en fs (= 10^(-15) s)
 
 //Pour l'argon
-#define E_0 119.8*1.380649*pow(10,-23)  //en kg.m²/s²
-#define d 3.405*pow(10,-9)    //en m
-#define m 6.6335*pow(10,-26)    //en kg (= 39,948 ± 0,001 u)
+#define E_0 9.960719*pow(10,-5)  //en u.Å²/fs² (= 119.8*1.380649*10^(-23) en kg.m²/s² )
+#define d 3.405    //en Å (1 Å = 10^(-10) m)
+#define m 39.948    //en u (1 u = 1.6605402E-27 kg)
 
-#define k_b 1.380649*pow(10,-23) // constante de Boltzman :	k = 1,380649 × 10−23 J.K−1
-#define temperature_cible 10    // degrés celsius
+#define k_b 8.314456*pow(10,-7) //en u.Å²/(fs².K) ( constante de Boltzman :	k = 1,380649 × 10−23 kg.m²/(s².K) )
+#define temperature_cible 300  // en K
 
-#define b_x 719.14*pow(10,-10)  //en m (= 719.14 Å)
-#define b_y 719.14*pow(10,-10)  //en m
-#define b_z 719.14*pow(10,-10)  //en m
+#define b_x 719.14  //en Å
+#define b_y 719.14  //en Å
+#define b_z 719.14  //en Å
