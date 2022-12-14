695019f22a29a33e1073d85274c62c129e324a61
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2022-12-14 19:37:26 +0100

    Update voisinlist.cpp

--- voisinlist.cpp
+++ voisinlist.cpp
@@ -1,50 +1,15 @@
 #include "voisinlist.h" 
 
-double getDet(const double* box)
-{
-  return box[0] * (box[4] * box[8] - box[5] * box[7]) +
-         box[1] * (box[5] * box[6] - box[3] * box[8]) +
-         box[2] * (box[3] * box[7] - box[4] * box[6]);
-}
 
-void getInverseBox(double* box)
-{
-  box[9] = box[4] * box[8] - box[5] * box[7];
-  box[10] = box[2] * box[7] - box[1] * box[8];
-  box[11] = box[1] * box[5] - box[2] * box[4];
-  box[12] = box[5] * box[6] - box[3] * box[8];
-  box[13] = box[0] * box[8] - box[2] * box[6];
-  box[14] = box[2] * box[3] - box[0] * box[5];
-  box[15] = box[3] * box[7] - box[4] * box[6];
-  box[16] = box[1] * box[6] - box[0] * box[7];
-  box[17] = box[0] * box[4] - box[1] * box[3];
-  double det = getDet(box);
-  for (int n = 9; n < 18; ++n) {
-    box[n] /= det;
-  }
-}
+// Type aliases to make the code more readable
+using Vecteur3D = Vecteur_3D;
+using Particules = struct Particules;
 
-void applyMicOne(double& x12)
-{
-  if (x12 < -0.5)
-    x12 += 1.0;
-  else if (x12 > +0.5)
-    x12 -= 1.0;
-}
-
-void applyMic(const double* box, double& x12, double& y12, double& z12)
-{
-  double sx12 = box[9] * x12 + box[10] * y12 + box[11] * z12;
-  double sy12 = box[12] * x12 + box[13] * y12 + box[14] * z12;
-  double sz12 = box[15] * x12 + box[16] * y12 + box[17] * z12;
-  applyMicOne(sx12);
-  applyMicOne(sy12);
-  applyMicOne(sz12);
-  x12 = box[0] * sx12 + box[1] * sy12 + box[2] * sz12;
-  y12 = box[3] * sx12 + box[4] * sy12 + box[5] * sz12;
-  z12 = box[6] * sx12 + box[7] * sy12 + box[8] * sz12;
-}
-
+// Struct to represent a pair of particle indices and their distance
+struct ParticlePair {
+     int i, j;
+  double distance;
+};
 
 
 
