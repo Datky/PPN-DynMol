c0265c578456efede5ae75d01ed5a7ccd63ce80a
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2022-12-14 19:50:29 +0100

    Update voisinlist.cpp

--- voisinlist.cpp
+++ voisinlist.cpp
@@ -1,15 +1,18 @@
 #include "voisinlist.h" 
 
 
-// Type aliases to make the code more readable
-using Vecteur3D = Vecteur_3D;
-using Particules = struct Particules;
 
 // Struct to represent a pair of particle indices and their distance
-struct ParticlePair {
-     int i, j;
-  double distance;
-};
 
+// Function to create the nearest neighbor table
 
+// Get pointers to the particle positions, velocities, and accelerations
+
+// Create an empty vector to hold the nearest neighbor table
+
+// Loop over all pairs of particles
+
+// Calculate the distance between the two particles
+
+// Add the particle pair to the nearest neighbor table
 
