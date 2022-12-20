4c0820286fb7578a0a95f6f5ba082270707e62bd
Joel Bougron <joel.bougron@ens.uvsq.fr>
2022-12-20 15:50:54 +0100

    Calcul direct de la force selon expression explicite de dU/dr.

--- potentiel.cpp
+++ potentiel.cpp
@@ -12,6 +12,9 @@ f64 Lennard_Jones(f64 const& r){
 
 f64 F_Lennard_Jones(f64 const& r){
       //F = - dU/dr
-      return 24*E_0*pow(d,6.0)*(( 2*pow(d,6.0) - pow(r,6.0) ) / pow(r,13.0));
+//      return 24*E_0*pow(d,6.0)*(( 2*pow(d,6.0) - pow(r,6.0) ) / pow(r,13.0));
+// BONNE EXPRESSION :
+        return 48*E_0/d*(pow(d/r,13.0)-1/2*pow(d/r,7.0));
 }
+
 //12*pow(d,12.0)/pow(r,13.0) - 6*pow(d,6.0)/pow(r,7.0) = 6*pow(d,6.0)*( 2*pow(d,6.0) - pow(r,6.0) ) / pow(r,13.0)
