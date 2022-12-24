66e49f43c562b7ad292bc93e6758d4a778443c62
Joel Bougron <joel.bougron@ens.uvsq.fr>
2022-12-24 14:37:19 +0100

    Force de Lennard-Jones : correction de la formule, abstention des cas où r<d.

--- potentiel.cpp
+++ potentiel.cpp
@@ -1,20 +1,25 @@
 /** @fichier
- * Les fonction d'éneregies potentiels de paire
+ * Les fonction d'énergies potentielles de paire
  **/
 
 #include "potentiel.h"
 
-// 
+// Calcul du potentiel de Lennard_Jones :
 f64 Lennard_Jones(f64 const& r){
-      return 4*E_0*(pow(d/r,12.0)-pow(d/r,6.0));
+    return 4*E_0*(pow(d/r,12.0)-pow(d/r,6.0));
 }
-//Optimisation 4*E_0*(pow(d,12.0)/pow(r²,6.0)-pow(d,6.0)/pow(r²,3.0));
+// ? Optimisation 4*E_0*(pow(d,12.0)/pow(r²,6.0)-pow(d,6.0)/pow(r²,3.0));
 
+// Calcul de la force de Lennard_Jones de manière explicite :
 f64 F_Lennard_Jones(f64 const& r){
-      //F = - dU/dr
-//      return 24*E_0*pow(d,6.0)*(( 2*pow(d,6.0) - pow(r,6.0) ) / pow(r,13.0));
-// BONNE EXPRESSION :
-        return 48*E_0/d*(pow(d/r,13.0)-1/2*pow(d/r,7.0));
+    f64 tmp_force; 
+    if (r<d) {
+        tmp_force = 0; // Abstention des cas où la force est répulsive.
+    } else {
+        tmp_force = -48*E_0/r*(pow(d/r,12.0)-1/2*pow(d/r,6.0)); // a priori ok : cf démo
+    } 
+    return tmp_force; // a priori ok : cf démo
 }
 
-//12*pow(d,12.0)/pow(r,13.0) - 6*pow(d,6.0)/pow(r,7.0) = 6*pow(d,6.0)*( 2*pow(d,6.0) - pow(r,6.0) ) / pow(r,13.0)
+// ? return 24*E_0*pow(d,6.0)*(( 2*pow(d,6.0) - pow(r,6.0) ) / pow(r,13.0));
+// ? 12*pow(d,12.0)/pow(r,13.0) - 6*pow(d,6.0)/pow(r,7.0) = 6*pow(d,6.0)*( 2*pow(d,6.0) - pow(r,6.0) ) / pow(r,13.0)
