3876bc242fd8be11f59cab3a4939d2acf5ffd878
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2022-12-26 02:15:41 +0100

    Update voisinlist.cpp

--- voisinlist.cpp
+++ voisinlist.cpp
@@ -1,58 +1,53 @@
 
-#include "voisinlist.h" 
+#include "voisinlist.h"
 
 
-// Struct to represent a pair of particle indices and their distance
+void  Voisin (Particles & at, f64 const & dt, f64 const & r_cut){
 
-struct Verlet {
-	int i, j;
-	double r_globale;
-};
+
 
 // Function to create the nearest neighbor table
-
-std::vector<Verlet> makeVoisinlist(Particules & particules) {
+	
+	std::vector<Particles> makeVoisinlist(Particules & particules) {
   
 
 // Get pointers to the particle positions, velocities, and accelerations
-  
-  
-	struct Vecteur_3D *__restrict positions = particules.pos;
-	struct Vecteur_3D *__restrict velocities = particules.vit;
-	struct Vecteur_3D *__restrict accelerations = particules.acc;
+		
+		struct Vecteur_3D *__restrict positions = particules.pos;
+		struct Vecteur_3D *__restrict velocities = particules.vit;
+		struct Vecteur_3D *__restrict accelerations = particules.acc;
 
 
 // Create an empty vector to hold the nearest neighbor table
-  
-  std::vector<Verlet> nearestVoisin;
+		
+		std::vector<Particles> nearestVoisin;
 
 // Loop over all pairs of particles
-  
- for (int i = 0; i < N; ++i) {
-		for (int j = i + 1; j < N; ++j){
+		
+		for (int i = 0; i < N; ++i) {
+			for (int j = i + 1; j < N; ++j){
             
   // Calculate the distance between the two particles          
-            f64 r_x = at.pos->X[i] - at.pos->X[j];
-            f64 r_y = at.pos->Y[i] - at.pos->Y[j];
-            f64 r_z = at.pos->Z[i] - at.pos->Z[j];
-            f64 r_globale = sqrt(pow(r_x,2.0) + pow(r_y,2.0) + pow(r_z,2.0));
+				f64 r_x = at.pos->X[i] - at.pos->X[j];
+				f64 r_y = at.pos->Y[i] - at.pos->Y[j];
+				f64 r_z = at.pos->Z[i] - at.pos->Z[j];
+				f64 r_globale = sqrt(pow(r_x,2.0) + pow(r_y,2.0) + pow(r_z,2.0));
                   
                  
  // Add the particle pair to the nearest neighbor table           
-            
-            nearestVoisin.push_back({i, j, r_globale}); }}
+				nearestVoisin.push_back({i, j, r_globale}); }}
 	
 	// Sort the nearest neighbor table in ascending order of distance
-	
-	std::sort(nearestVoisin.begin(), nearestVoisin.end(), [](const Verlet& a, const Verlet& b) { 
 		
-		return a.r_globale < b.r_globale; });
+		std::sort(nearestVoisin.begin(), nearestVoisin.end(), [](const Particles& a, const Particles& b) { 
+			
+			return a.r_globale < b.r_globale; });
 	
 	// Return the nearest neighbor table
-	
-	return nearestVoisin;
-}
+		
+		return nearestVoisin;
+	}
 
-				
+}			
   
   
