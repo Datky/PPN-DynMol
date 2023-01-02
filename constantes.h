48e5714bd77833d2e1e3828e20b31702dffa4534
Joel Bougron <joel.bougron@ens.uvsq.fr>
2023-01-02 22:36:35 +0100

    Ajout test pour vérifier que les particules ne sont pas trop proches et supprimer les ‘nan'.

--- constantes.h
+++ constantes.h
@@ -3,6 +3,7 @@
 #define N 10000
 #define nb_iteration 20
 #define dt 1000   //en fs (= 10^(-15) s)
+#define proches 1 // Si la distance est < 1 Å, les particules sont considérées trop proches et le potentiel négligé
 
 //Pour l'argon
 #define E_0 9.960719*pow(10,-5)  //en u.Å²/fs² (= 119.8*1.380649*10^(-23) en kg.m²/s² )
