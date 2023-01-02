985e84802eb350da4ac3308ed48e96b3d77459cd
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-01-02 13:29:21 +0100

    Update rayonverlet.cpp

--- rayonverlet.cpp
+++ rayonverlet.cpp
@@ -1,23 +1,24 @@
 
+#include "constantes.h"
 #include <iostream>
 #include "main.cpp"
+#include "types.h"
 
-
-
-void rayonverlet1(const double Length, const double halfLength, double& x12)
+void rayonverlet1(const double Length, const double halfLength, double& xij)
 {
-  if (x12 < -halfLength)
-    x12 += length;
-  else if (x12 > +halfLength)
-    x12 -= length;
+	if (xij < -halfLength)
+		xij += Length;
+	else if (xij > +halfLength)
+		xij -= Length;
 }
 
-void rayonverlet(const double b_x, const double b_y, const double b_z, double& x12, double& y12, double& z12)
+
+void rayonverlet(const double bx, const double by, const double bz, double& x12, double& y12, double& z12)
 {
-    const double halfx = b_x * 0.5;
-    const double halfy = b_y * 0.5;
-    const double halfz = b_z * 0.5;
-    rayonverlet1(b_x, halfx, x12);
-    rayonverlet1(b_y, halfy, y12);
-    rayonverlet1(b_z, halfz, z12);
+	const double halfx = bx * 0.5;
+	const double halfy = by * 0.5;
+	const double halfz = bz * 0.5;
+	rayonverlet1(b_x, halfx, x12);
+	rayonverlet1(b_y, halfy, y12);
+	rayonverlet1(b_z, halfz, z12);
 }
