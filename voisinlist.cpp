aecef4a8cbe9b80d9281c30af35d92b5a0757719
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-01-02 18:34:11 +0100

    Update voisinlist.cpp

--- voisinlist.cpp
+++ voisinlist.cpp
@@ -2,52 +2,55 @@
 #include "voisinlist.h"
 
 
-void  Voisin (Particules & at, f64 const & dt, f64 const & r_cut){
+using namespace std;
+using Particules = struct Particules;
+
+struct Particules{
+
+	int i, j;
+
+	double r_globale;
+
+};
 
 
+void  Voisin(Particules& at, f64 const& r_cut) {
 
-// Function to create the nearest neighbor table
-	
-	std::vector<Particules> makeVoisinlist(Particules & particules) {
-  
+	struct Particules particules;
 
-// Get pointers to the particle positions, velocities, and accelerations
-		
-		struct Vecteur_3D *__restrict positions = particules.pos;
-		struct Vecteur_3D *__restrict velocities = particules.vit;
-		struct Vecteur_3D *__restrict accelerations = particules.acc;
+	std::vector<Particules> makeVoisinlist(Particules & particules);
 
+		const Vecteur_3D* __restrict positions = particules.pos;
+		const Vecteur_3D* __restrict velocities = particules.vit;
+		const Vecteur_3D* __restrict accelerations = particules.acc;
 
-// Create an empty vector to hold the nearest neighbor table
-		
 		std::vector<Particules> nearestVoisin;
 
-// Loop over all pairs of particles
-		
 		for (int i = 0; i < N; ++i) {
-			for (int j = i + 1; j < N; ++j){
-            
-  // Calculate the distance between the two particles          
-				f64 r_x = at.pos->X[i] - at.pos->X[j];
-				f64 r_y = at.pos->Y[i] - at.pos->Y[j];
-				f64 r_z = at.pos->Z[i] - at.pos->Z[j];
-				f64 r_globale = sqrt(pow(r_x,2.0) + pow(r_y,2.0) + pow(r_z,2.0));
-                  
-                 
- // Add the particle pair to the nearest neighbor table           
-				nearestVoisin.push_back({i, j, r_globale}); }}
-	
-	// Sort the nearest neighbor table in ascending order of distance
-		
-		std::sort(nearestVoisin.begin(), nearestVoisin.end(), [](const Particules& a, const Particules& b) { 
-			
-			return a.r_globale < b.r_globale; });
-	
-	// Return the nearest neighbor table
-		
+			for (int j = i + 1; j < N; ++j) {
+
+				// Calculate the distance between the two particles          
+				const double r_x = abs(at.pos->X[j] - at.pos->X[i]);
+				const double r_y = abs(at.pos->Y[j] - at.pos->Y[i]);
+				const double r_z = abs(at.pos->Z[j] - at.pos->Z[i]);
+				
+				const double r_globale = sqrt(pow(r_x, 2.0) + pow(r_y, 2.0) + pow(r_z, 2.0));
+     
+				nearestVoisin.push_back({ i, j, r_globale });
+			}
+		}
+
+		// Sort the nearest neighbor table in ascending order of distance
+
+		std::sort(nearestVoisin.begin(), nearestVoisin.end(), [](const Particules& a, const Particules& b) {
+
+			return a.r_globale < b.r_globale;
+
+			}
+
+		// Return the nearest neighbor table
+
 		return nearestVoisin;
 	}
 
-}			
-  
   
