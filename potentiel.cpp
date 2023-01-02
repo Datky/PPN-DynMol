d036bf6182f3576734500f78096cbdae36203806
Joel Bougron <joel.bougron@ens.uvsq.fr>
2023-01-02 22:51:25 +0100

    Ajout test pour vérifier que les particules ne sont pas trop proches et supprimer les ‘nan'. Signe ‘-’ devant le potentiel.

--- potentiel.cpp
+++ potentiel.cpp
@@ -8,10 +8,10 @@
 // Calcul de la force de Lennard_Jones de manière explicite :
 f64 F_Lennard_Jones(f64 const& r){
     f64 tmp_force; 
-    if (r<d) {
-        tmp_force = 0; // Abstention des cas où la force est répulsive.
+    if (r<proches) {
+        tmp_force = 0; // NEW Abstention des cas où les particules sont trop proches
     } else {
-        tmp_force = 48*E_0/r*(pow(d/r,12.0)-1/2*pow(d/r,6.0)); // a priori ok : cf démo
+        tmp_force = -48*E_0/r*(pow(d/r,12.0)-1/2*pow(d/r,6.0)); // ok : cf démo
     } 
     return tmp_force; // a priori ok : cf démo
 }
