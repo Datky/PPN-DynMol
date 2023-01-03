8d2377b00224113a86248e5c0ac636875ea049b5
Joel Bougron <joel.bougron@ens.uvsq.fr>
2023-01-03 15:26:26 +0100

    Structures, données, méthodes pour les paramètres ‘N’, ‘nb_iteration’, et ‘dt’. Pragma once (au lieu des include guards), suppression d’includes.

--- constantes.h
+++ constantes.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#define N 10000
-#define nb_iteration 20
-#define dt 1000   //en fs (= 10^(-15) s)
+#include "types.h"
 #define proches 1 // Si la distance est < 1 Å, les particules sont considérées trop proches et le potentiel négligé
+/*
+#define N 10000
+#define nb_iteration 5 //NEW
+#define dt 1000   //en fs (= 10^(-15) s)
+*/
 
 //Pour l'argon
 #define E_0 9.960719*pow(10,-5)  //en u.Å²/fs² (= 119.8*1.380649*10^(-23) en kg.m²/s² )
@@ -16,3 +19,23 @@
 #define b_x 719.14  //en Å
 #define b_y 719.14  //en Å
 #define b_z 719.14  //en Å
+
+extern const u32 N;
+extern const u32 nb_iteration;
+extern const u32 dt;
+
+struct parametres_N {
+    u32 N;
+    u32 parametres();
+};
+
+struct parametres_nb_iteration {
+    u32 nb_iteration;
+    u32 parametres();
+};
+
+struct parametres_dt {
+    u32 dt;
+    u32 parametres();
+};
+
