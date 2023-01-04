635bb4081371b9b0790ec5bc03af571d962f5445
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-01-04 17:41:02 +0100

    Update rayonverlet.cpp

--- rayonverlet.cpp
+++ rayonverlet.cpp
@@ -6,19 +6,18 @@
 
 void rayonverlet1(const double boxLength, const double halfLength, double& xij)
 {
-	if (xij < -halfLength)
-		xij += boxLength;
-	else if (xij > +halfLength)
-		xij -= boxLength;
+        if (xij < -halfLength)
+                xij += boxLength;
+        else if (xij > +halfLength)
+                xij -= boxLength;
 }
 
 
 void rayonverlet(const double bx, const double by, const double bz, double& x12, double& y12, double& z12)
 {
-	const double halfx = bx * 0.5;
-	const double halfy = by * 0.5;
-	const double halfz = bz * 0.5;
-	rayonverlet1(b_x, halfx, x12);
-	rayonverlet1(b_y, halfy, y12);
-	rayonverlet1(b_z, halfz, z12);
-}
+        const double halfx = bx * 0.5;
+        const double halfy = by * 0.5;
+        const double halfz = bz * 0.5;
+        rayonverlet1(b_x, halfx, x12);
+        rayonverlet1(b_y, halfy, y12);
+-- INSERT --                                                  1,1           Top
