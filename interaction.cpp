d9e86e9c1fffc78c7ec8c3e8dc7bd9355de04dc9
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-01-04 17:39:12 +0100

    Update interaction.cpp

--- interaction.cpp
+++ interaction.cpp
@@ -5,7 +5,42 @@
 #include <cmath>
 #include "interaction.h"
 #include "rayonverlet.cpp"
-#include "voisinlist.cpp"
+#include <vector>
+
+std::vector<int> NN, NL;
+const int MN = 10000;
+
+void  Voisin(Particules & at, f64 const& r_cut) {
+
+        double r_cut2 = r_cut * r_cut;
+
+        std::fill(NN.begin(), NN.end(), 0);
+
+                for (int i = 0; i < N; ++i) {
+                        for (int j = i + 1; j < N; ++j) {
+
+                                // Calculate the distance between the two particles          
+                                double r_x = abs(at.pos->X[j] - at.pos->X[i]);
+                                double r_y = abs(at.pos->Y[j] - at.pos->Y[i]);
+                                double r_z = abs(at.pos->Z[j] - at.pos->Z[i]);
+                                rayonverlet(b_x, b_y, b_z, r_x, r_y, r_z);
+                                const double r_global = sqrt(pow(r_x, 2.0) + pow(r_y, 2.0) + pow(r_z, 2.0));
+                  if (r_global < r_cut && r_global != 0) {
+
+                                        NL[i * MN + NN[i]++] = j;
+                                        if (NN[i] > MN) {
+                                                std::cout << "Erreur : nombre de voisins pour l'atome " << i << " exceeds " << MN << std::endl;
+                                                exit(1);
+
+                                        }
+
+
+                                        }
+
+                        }
+                }
+}
+            
 
 // Algorithme de Verlet
 void Verlet(Particules & at, f64 const& r_cut, Frontiere const& frontiere_type){
