abc4c0a6786a05baa779b1d454e1cc4543f067cc
pasquiermateo33@gmail.com <iamsquel@gmail.com>
2023-01-05 10:14:21 +0100

    Ajout  de la structure AoS.
    Pour calcule des performances différentes.

---
+++ AoS/particule.h
@@ -0,0 +1,31 @@
+#pragma once
+//#ifndef STRUCT_PA
+//#define STRUCT_PA
+
+#include <iostream>
+#include <cstdlib>
+#include "../types.h"
+
+/** @fichier
+ * Structures de données pour représenter les particules
+ **/
+
+
+struct Particule {
+      f64 *__restrict pos_X;
+      f64 *__restrict pos_Y;
+
+      f64 *__restrict vit_X;
+      f64 *__restrict vit_Y;
+
+      f64 *__restrict acc_X;
+      f64 *__restrict acc_Y;
+};
+
+
+struct Liste {
+      Particule *__restrict liste;
+};
+
+
+//#endif //STRUCT_PA
