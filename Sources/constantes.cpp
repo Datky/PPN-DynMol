2b7d28421376c579a6b85c565cbe61c435345105
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-05-06 23:46:43 +0200

    Add files via upload

---
+++ Sources/constantes.cpp
@@ -0,0 +1,33 @@
+#include "../Headers/constantes.h"
+#include <iostream>
+
+/* NEW :
+u32 parametres_N::parametres() {
+    std::cout << std::endl << "Merci d'entrer le nombre de particules N (5000 ou 10000 recommandés ou 15000): ";
+    std::cin >> N;
+    std::cout << std::endl;
+    return N;
+}
+parametres_N objet_N {};
+
+u32 parametres_nb_iteration::parametres() {
+    std::cout << std::endl << "Merci d'entrer nb_iteration (exemple : 20): ";
+    std::cin >> nb_iteration;
+    std::cout << std::endl;
+    return nb_iteration;
+}
+parametres_nb_iteration objet_nb_iteration {};
+
+u32 parametres_dt::parametres() {
+    std::cout << std::endl << "Merci d'entrer dt(en fs ; exemple : 10 fs): ";
+    std::cin >> dt;
+    std::cout << std::endl;
+    return dt;
+}
+parametres_dt objet_dt {};
+
+const u32 N = objet_N.parametres(); // NEW
+const u32 nb_iteration = objet_nb_iteration.parametres();
+const u32 dt = objet_dt.parametres();
+*/
+
