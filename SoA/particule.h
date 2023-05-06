82e22c8fe84977b180d3f08a33241ed504f93933
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-05-06 23:48:24 +0200

    Add files via upload

---
+++ SoA/particule.h
@@ -0,0 +1,29 @@
+#pragma once
+//#ifndef STRUCT_P
+//#define STRUCT_P
+
+#include <iostream>
+#include <cstdlib>
+#include "../Headers/types.h"
+
+/** @fichier
+ * Structures de données pour représenter les particules
+ **/
+
+
+struct Vecteur_3D {
+      f64 *__restrict X;
+      f64 *__restrict Y;
+      f64 *__restrict Z;
+};
+
+
+struct Particules {
+      Vecteur_3D *__restrict pos;
+      Vecteur_3D *__restrict prev_pos;
+      Vecteur_3D *__restrict vit;
+      Vecteur_3D *__restrict acc;
+};
+
+
+//#endif //STRUCT_P
