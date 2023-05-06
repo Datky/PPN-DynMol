6f5521bc742acfff91b962c3ac582205e9f38346
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-05-06 23:49:05 +0200

    Add files via upload

---
+++ AoS/particule.h
@@ -0,0 +1,34 @@
+#pragma once
+//#ifndef STRUCT_PA
+//#define STRUCT_PA
+
+#include <iostream>
+#include <cstdlib>
+#include "../Headers/types.h"
+
+/** @fichier
+ * Structures de données pour représenter les particules
+ **/
+
+
+struct Particule {
+      f64 *__restrict pos_X;
+      f64 *__restrict pos_Y;
+      f64 *__restrict pos_Z;
+
+      f64 *__restrict vit_X;
+      f64 *__restrict vit_Y;
+      f64 *__restrict vit_Z;
+
+      f64 *__restrict acc_X;
+      f64 *__restrict acc_Y;
+      f64 *__restrict acc_Z;
+};
+
+
+struct Liste {
+      Particule *__restrict liste;
+};
+
+
+//#endif //STRUCT_PA
