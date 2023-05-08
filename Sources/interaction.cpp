78aff260989f36a2d92382aed82c59ef86b95fec
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-05-08 04:31:28 +0200

    Update interaction.cpp

--- Sources/interaction.cpp
+++ Sources/interaction.cpp
@@ -16,41 +16,9 @@ extern u32 b_y; // NEW
 extern u32 b_z; // NEW
 extern u32  MN;
 //const int MN = 10000;
-/*
-void  Voisin(Particules & at, f64 const& r_cut) {
-
-        double r_cut2 = r_cut * r_cut;
-
-        std::fill(NN.begin(), NN.end(), 0);
-
-                for (int i = 0; i < N; ++i) {
-                        for (int j = i + 1; j < N; ++j) {
-
-                                // Calculate the distance between the two particles          
-                                double r_x = abs(at.pos->X[j] - at.pos->X[i]);
-                                double r_y = abs(at.pos->Y[j] - at.pos->Y[i]);
-                                double r_z = abs(at.pos->Z[j] - at.pos->Z[i]);
-                                rayonverlet(b_x, b_y, b_z, r_x, r_y, r_z);
-                                const double r_global = sqrt(pow(r_x, 2.0) + pow(r_y, 2.0) + pow(r_z, 2.0));
-                  if (r_global < r_cut && r_global != 0) {
-
-                                        NL[i * MN + NN[i]++] = j;
-                                        if (NN[i] > MN) {
-                                                std::cout << "Erreur : nombre de voisins pour l'atome " << i << " exceeds " << MN << std::endl;
-                                                exit(1);
-
-                                        }
 
 
-                                        }
-
-                        }
-                }
-}
-*/           
-
-
-void majPositionsetCellules(std::vector<std::vector<std::vector<std::vector<u32>>>> &vec, Particules &at, f64 const& r_cut_carre, Frontiere const& frontiere_type/*, f64 max_displacement*/) {
+void majPositionsetCellules(std::vector<std::vector<std::vector<std::vector<u32>>>> &vec, Particules &at, f64 const& r_cut_carre, Frontiere const& frontiere_type) {
 
       f64 F_x, F_y, F_z ;
       F_x = F_y = F_z = 0; // ????
